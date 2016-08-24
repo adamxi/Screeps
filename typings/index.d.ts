c8890838a52261373b47adc722fbebe6dbc7c383
Adam Rene Gregersen <arg@netcompany.com>
2016-08-24 20:59:49 +0200

    vip

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="extensions/index.d.ts" />
 /// <reference path="globals/lodash/index.d.ts" />
 /// <reference path="globals/screeps/index.d.ts" />
+/// <reference path='../node_modules/typescript/lib/lib.es6.d.ts'/>
